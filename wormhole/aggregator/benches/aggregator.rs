83b05ff642934f958fd52d70123925366044ff73
zeapoz <zeapo@pm.me>
2025-06-23 20:49:07 +0200

    feat: benches for verifying aggregated proofs

--- wormhole/aggregator/benches/aggregator.rs
+++ wormhole/aggregator/benches/aggregator.rs
@@ -6,6 +6,13 @@ use zk_circuits_common::circuit::{C, D, F};
 
 const DUMMY_PROOF_BYTES: &[u8] = include_bytes!("../data/dummy_proof_zk.bin");
 
+fn deserialize_proofs<const N: usize>(
+    common_data: &CommonCircuitData<F, D>,
+) -> [ProofWithPublicInputs<F, C, D>; N] {
+    let proof = ProofWithPublicInputs::from_bytes(DUMMY_PROOF_BYTES.to_vec(), common_data).unwrap();
+    std::array::from_fn(|_| proof.clone())
+}
+
 // A macro for creating an aggregation benchmark with a specified number of proofs to
 // aggregate. The number of proofs is expected to be some constant, N, that can be expressed as `2^M`.
 macro_rules! aggregate_proofs_benchmark {
@@ -41,11 +48,40 @@ macro_rules! aggregate_proofs_benchmark {
     };
 }
 
-fn deserialize_proofs<const N: usize>(
-    common_data: &CommonCircuitData<F, D>,
-) -> [ProofWithPublicInputs<F, C, D>; N] {
-    let proof = ProofWithPublicInputs::from_bytes(DUMMY_PROOF_BYTES.to_vec(), common_data).unwrap();
-    std::array::from_fn(|_| proof.clone())
+macro_rules! verify_aggregate_proof_benchmark {
+    ($fn_name:ident, $num_proofs:expr) => {
+        pub fn $fn_name(c: &mut Criterion) {
+            const N: usize = $num_proofs;
+
+            // Setup proofs.
+            let proofs = {
+                let temp_aggregator = WormholeProofAggregator::<N>::default();
+                deserialize_proofs::<N>(&temp_aggregator.leaf_circuit_data.common)
+            };
+
+            c.bench_function(
+                &format!("verify_aggregate_proof_{}", stringify!($num_proofs)),
+                |b| {
+                    b.iter_batched(
+                        || {
+                            let mut aggregator = WormholeProofAggregator::<N>::default();
+                            for proof in proofs.clone() {
+                                aggregator.push_proof(proof).unwrap();
+                            }
+
+                            aggregator.aggregate().unwrap()
+                        },
+                        |aggregated_proof| {
+                            let proof = aggregated_proof.proof;
+                            let circuit_data = aggregated_proof.circuit_data;
+                            circuit_data.verify(proof).unwrap();
+                        },
+                        criterion::BatchSize::SmallInput,
+                    );
+                },
+            );
+        }
+    };
 }
 
 // Various proof sizes.
@@ -55,10 +91,17 @@ aggregate_proofs_benchmark!(bench_aggregate_8_proofs, 8);
 aggregate_proofs_benchmark!(bench_aggregate_16_proofs, 16);
 aggregate_proofs_benchmark!(bench_aggregate_32_proofs, 32);
 
+verify_aggregate_proof_benchmark!(bench_verify_aggregate_proof_2, 2);
+verify_aggregate_proof_benchmark!(bench_verify_aggregate_proof_4, 4);
+verify_aggregate_proof_benchmark!(bench_verify_aggregate_proof_8, 8);
+verify_aggregate_proof_benchmark!(bench_verify_aggregate_proof_16, 16);
+verify_aggregate_proof_benchmark!(bench_verify_aggregate_proof_32, 32);
+
 criterion_group!(
     name = benches;
     config = Criterion::default()
         .sample_size(10);
-    targets = bench_aggregate_2_proofs, bench_aggregate_4_proofs, bench_aggregate_8_proofs, bench_aggregate_16_proofs, bench_aggregate_32_proofs
+    targets = bench_aggregate_2_proofs, bench_aggregate_4_proofs, bench_aggregate_8_proofs, bench_aggregate_16_proofs, bench_aggregate_32_proofs,
+              bench_verify_aggregate_proof_2, bench_verify_aggregate_proof_4, bench_verify_aggregate_proof_8, bench_verify_aggregate_proof_16, bench_verify_aggregate_proof_32,
 );
 criterion_main!(benches);
