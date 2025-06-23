2f8e5353cecde431569f221209a25e04e3e2e285
zeapoz <zeapo@pm.me>
2025-06-23 20:04:44 +0200

    chore: initialize aggregator before benching

--- wormhole/aggregator/benches/aggregator.rs
+++ wormhole/aggregator/benches/aggregator.rs
@@ -1,12 +1,9 @@
-use std::time::Duration;
-
 use criterion::{criterion_group, criterion_main, Criterion};
 use plonky2::plonk::circuit_data::CommonCircuitData;
 use wormhole_aggregator::aggregator::WormholeProofAggregator;
 use wormhole_verifier::ProofWithPublicInputs;
 use zk_circuits_common::circuit::{C, D, F};
 
-const MEASUREMENT_TIME_S: u64 = 100;
 const DUMMY_PROOF_BYTES: &[u8] = include_bytes!("../data/dummy_proof_zk.bin");
 
 // A macro for creating an aggregation benchmark with a specified number of proofs to
@@ -16,20 +13,28 @@ macro_rules! aggregate_proofs_benchmark {
         pub fn $fn_name(c: &mut Criterion) {
             const N: usize = $num_proofs;
 
+            // Setup proofs.
+            let proofs = {
+                let temp_aggregator = WormholeProofAggregator::<N>::default();
+                deserialize_proofs::<N>(&temp_aggregator.leaf_circuit_data.common)
+            };
+
             c.bench_function(
                 &format!("aggregate_proofs_{}", stringify!($num_proofs)),
                 |b| {
-                    b.iter(|| {
-                        let mut aggregator = WormholeProofAggregator::<N>::default();
-
-                        let proofs = deserialize_proofs::<N>(&aggregator.leaf_circuit_data.common);
-
-                        for proof in proofs.clone() {
-                            aggregator.push_proof(proof).unwrap();
-                        }
-
-                        aggregator.aggregate().unwrap();
-                    });
+                    b.iter_batched(
+                        || {
+                            let mut aggregator = WormholeProofAggregator::<N>::default();
+                            for proof in proofs.clone() {
+                                aggregator.push_proof(proof).unwrap();
+                            }
+                            aggregator
+                        },
+                        |mut aggregator| {
+                            aggregator.aggregate().unwrap();
+                        },
+                        criterion::BatchSize::SmallInput,
+                    );
                 },
             );
         }
@@ -50,11 +55,9 @@ aggregate_proofs_benchmark!(bench_aggregate_8_proofs, 8);
 aggregate_proofs_benchmark!(bench_aggregate_16_proofs, 16);
 aggregate_proofs_benchmark!(bench_aggregate_32_proofs, 32);
 
-// TODO: Add function to only bench circuit.
 criterion_group!(
     name = benches;
     config = Criterion::default()
-        .measurement_time(Duration::from_secs(MEASUREMENT_TIME_S))
         .sample_size(10);
     targets = bench_aggregate_2_proofs, bench_aggregate_4_proofs, bench_aggregate_8_proofs, bench_aggregate_16_proofs, bench_aggregate_32_proofs
 );
