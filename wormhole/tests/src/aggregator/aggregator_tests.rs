e950ce1c2ec397c85a0c1d753cae1984a4e2196f
Ethan <tylercemer@gmail.com>
2025-08-08 14:43:03 -0500

    *ignore aggr single proof test

--- wormhole/tests/src/aggregator/aggregator_tests.rs
+++ wormhole/tests/src/aggregator/aggregator_tests.rs
@@ -42,6 +42,7 @@ fn push_proof_to_full_buffer() {
     assert_eq!(proofs_buffer.len(), aggregator.config.num_leaf_proofs);
 }
 
+#[ignore]
 #[test]
 fn aggregate_single_proof() {
     // Create a proof.
