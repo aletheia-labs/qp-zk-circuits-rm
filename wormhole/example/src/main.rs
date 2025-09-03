e27ae06056e20cfcb32665519fbcf86f8029dbbc
Ethan <tylercemer@gmail.com>
2025-09-03 00:34:13 -0500

    use the same exit account in wormhole chain CI tests.

--- wormhole/example/src/main.rs
+++ wormhole/example/src/main.rs
@@ -1,22 +1,24 @@
-use plonky2::field::types::Field;
 use plonky2::hash::poseidon::PoseidonHash;
 use plonky2::plonk::circuit_data::CircuitConfig;
 use plonky2::plonk::config::Hasher;
+use plonky2::plonk::proof::ProofWithPublicInputs;
 use wormhole_circuit::inputs::{CircuitInputs, PrivateCircuitInputs, PublicCircuitInputs};
 use wormhole_circuit::nullifier::Nullifier;
 use wormhole_circuit::storage_proof::ProcessedStorageProof;
 use wormhole_circuit::substrate_account::SubstrateAccount;
 use wormhole_circuit::unspendable_account::UnspendableAccount;
 use wormhole_prover::WormholeProver;
-use zk_circuits_common::circuit::F;
 use zk_circuits_common::utils::{digest_felts_to_bytes, u128_to_felts, u64_to_felts};
 
 fn main() -> anyhow::Result<()> {
     // Create inputs. In practice, each input would be gathered from the real node.
-    let funding_account = SubstrateAccount::new(&[2u8; 32])?;
+    let funding_account = SubstrateAccount::new(&[
+        226, 124, 203, 9, 80, 60, 124, 205, 165, 5, 178, 216, 195, 15, 149, 38, 116, 1, 238, 133,
+        181, 154, 106, 17, 41, 228, 118, 179, 82, 141, 225, 76,
+    ])?; // Alice's dev account
     let secret = [1u8; 32];
     let unspendable_account = UnspendableAccount::from_secret(&secret).account_id;
-    let funding_amount = 1_000_000_000u128;
+    let funding_amount = 1_000_000_000_000u128;
     let transfer_count = 0u64;
 
     let mut leaf_inputs_felts = Vec::new();
@@ -27,7 +29,13 @@ fn main() -> anyhow::Result<()> {
     let leaf_inputs_hash = PoseidonHash::hash_no_pad(&leaf_inputs_felts);
     let root_hash = digest_felts_to_bytes(leaf_inputs_hash.elements);
 
-    let exit_account = SubstrateAccount::new(&[2u8; 32])?;
+    let exit_account_id = 8226349481601990196u64;
+    let exit_account_bytes = exit_account_id.to_le_bytes();
+    // pad with 0 bytes to make it 32 bytes long
+    let mut exit_account_bytes = exit_account_bytes.to_vec();
+    exit_account_bytes.resize(32, 0);
+
+    let exit_account = SubstrateAccount::new(&exit_account_bytes)?;
 
     let inputs = CircuitInputs {
         private: PrivateCircuitInputs {
@@ -38,7 +46,7 @@ fn main() -> anyhow::Result<()> {
             unspendable_account: (unspendable_account).into(),
         },
         public: PublicCircuitInputs {
-            funding_amount: funding_amount,
+            funding_amount,
             nullifier: Nullifier::from_preimage(&secret, 0).hash.into(),
             root_hash,
             exit_account: (*exit_account).into(),
@@ -48,7 +56,11 @@ fn main() -> anyhow::Result<()> {
     let config = CircuitConfig::standard_recursion_config();
     let prover = WormholeProver::new(config);
     let prover_next = prover.commit(&inputs)?;
-    let proof = prover_next.prove().expect("proof failed; qed");
+    let proof: ProofWithPublicInputs<
+        plonky2::field::goldilocks_field::GoldilocksField,
+        plonky2::plonk::config::PoseidonGoldilocksConfig,
+        2,
+    > = prover_next.prove().expect("proof failed; qed");
 
     let public_inputs = PublicCircuitInputs::try_from(&proof)?;
     // print the public inputs
